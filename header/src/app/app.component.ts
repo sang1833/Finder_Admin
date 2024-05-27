94ba4ed4fb919512de87e020687ad56ffdf2dd01
Sang <75106540+sang1833@users.noreply.github.com>
2024-05-27 18:06:47 +0700

    handle button logout in header; change deprecated library CanActivate to CanActivateFn

--- header/src/app/app.component.ts
+++ header/src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import logo from '../assets/angular.png'
-import avatar from '../assets/avatar.png'
+import logo from '../assets/angular.png';
+import avatar from '../assets/avatar.png';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  myLogo = logo
-  myAvatar = avatar
+  myLogo = logo;
+  myAvatar = avatar;
+
+  logOut() {
+    // if localstorage have item user, it will call api http://localhost:5001/api/v1/auths/logout
+    // then remove item user from localstorage
+    // and redirect to login page
+    // else redirect to login page
+    if (localStorage.getItem('user')) {
+      const user = localStorage.getItem('user');
+      if (!user) return console.log('No user found');
+      fetch('http://localhost:5001/api/v1/auths/logout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${user}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data);
+          localStorage.removeItem('user');
+          window.location.href = '/login';
+        })
+        .catch((err) => {
+          console.log('Error in header remote', err);
+          localStorage.removeItem('user');
+          window.location.href = '/login';
+        });
+    } else {
+      window.location.href = '/login';
+    }
+  }
 }
